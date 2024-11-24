9926ab77586d10b83cea0ee4982b6435b56f571e
nariguela <pedrodc3048@gmail.com>
2024-11-24 12:23:35 -0300

    Feito o componente de adicionar tarefa aparecer/ desaparecer com o clique do botão

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
+import { useState } from "react";
 import AddToDoItem from "./components/AddToDoItem";
 import Button from "./components/Button";
 import Header from "./components/Header";
 import List from "./components/ToDoList";
 
 export default function App() {
+  const [showAddToDoItem, setShowAddToDoItem] = useState(false);
+
+  function handleAddToDoItem() {
+    setShowAddToDoItem((show) => !show);
+  }
+
   return (
     <>
       <Header />
       <div className="app">
         <div>
           <List />
-          <AddToDoItem />
-          <Button>Adicionar</Button>
+          {showAddToDoItem && <AddToDoItem />}
+          <Button onClick={handleAddToDoItem}>
+            {showAddToDoItem ? "Cancelar" : "Adicionar"}
+          </Button>
         </div>
       </div>
     </>
