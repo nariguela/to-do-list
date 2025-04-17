e0d121fea5be2a1964e6e8936b3638d53c8fc071
Pedro <pedrodc3048@gmail.com>
2025-04-16 22:51:51 -0300

    Removido dados iniciais de teste

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,20 +1,7 @@
 import React from "react";
 import ToDoItem from "./ToDoItem";
 
-export const toDoList = [
-  {
-    id: 1,
-    name: "Ir no poupatempo",
-    dueDate: "23/11/2024",
-    completed: false,
-  },
-  {
-    id: 2,
-    name: "Lavar o carro",
-    dueDate: "24/11/2024",
-    completed: false,
-  },
-];
+export const toDoList = [];
 
 export default function ToDoList({ toDoItems, onToggleItem, onDeleteItem }) {
   return (
